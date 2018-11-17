030d8e41c9fb691c254764d73281e73e1432cc21
Matthew Moskewicz <moskewcz@alumni.princeton.edu>
2018-11-16 21:46:44 -0800

    tabify and minor fixups
    in particular, use serial timeout, read all 32 bytes, and remove ord() calls (python3 compat)

--- pms7003.py
+++ pms7003.py
@@ -53,63 +53,63 @@ import os
 
 physicalPort = '/dev/ttyUSB0'
 
-serialPort = serial.Serial(physicalPort)  # open serial port
-
+serialPort = serial.Serial(port=physicalPort, baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
+                           stopbits=serial.STOPBITS_ONE, timeout=.7)
 while True:
-	# Check if we have enough data to read a payload
-	if serialPort.in_waiting >= 32:
-		# Check that we are reading the payload from the correct place (i.e. the start bits)
-		if ord(serialPort.read()) == 0x42 and ord(serialPort.read()) == 0x4d:
+        # Check if we have enough data to read a payload
+        print("waiting:", serialPort.in_waiting)
+        data_full = serialPort.read(32)
 
-			# Read the remaining payload data
-			data = serialPort.read(30)
-			# Extract the byte data by summing the bit shifted high byte with the low byte
-			# Use ordinals in python to get the byte value rather than the char value
-			frameLength = ord(data[1]) + (ord(data[0])<<8)
-			# Standard particulate values in ug/m3
-			concPM1_0_CF1 = ord(data[3]) + (ord(data[2])<<8)
-			concPM2_5_CF1 = ord(data[5]) + (ord(data[4])<<8)
-			concPM10_0_CF1 = ord(data[7]) + (ord(data[6])<<8)
-			# Atmospheric particulate values in ug/m3
-			concPM1_0_ATM = ord(data[9]) + (ord(data[8])<<8)
-			concPM2_5_ATM = ord(data[11]) + (ord(data[10])<<8)
-			concPM10_0_ATM = ord(data[13]) + (ord(data[12])<<8)
-			# Raw counts per 0.1l
-			rawGt0_3um = ord(data[15]) + (ord(data[14])<<8)
-			rawGt0_5um = ord(data[17]) + (ord(data[16])<<8)
-			rawGt1_0um = ord(data[19]) + (ord(data[18])<<8)
-			rawGt2_5um = ord(data[21]) + (ord(data[20])<<8)
-			rawGt5_0um = ord(data[23]) + (ord(data[22])<<8)
-			rawGt10_0um = ord(data[25]) + (ord(data[24])<<8)
-			# Misc data
-			version = ord(data[26])
-			errorCode = ord(data[27])
-			payloadChecksum = ord(data[29]) + (ord(data[28])<<8)
+        # Check that we are reading a full payload from the correct place (i.e. the start bits)
+        if (len(data_full) >= 32) and (data_full[0] == 0x42) and (data_full[1] == 0x4d):
+                # Read the remaining payload data
+                data = data_full[2:]
+                # Extract the byte data by summing the bit shifted high byte with the low byte
+                # Use ordinals in python to get the byte value rather than the char value
+                frameLength = data[1] + (data[0]<<8)
+                # Standard particulate values in ug/m3
+                concPM1_0_CF1 = data[3] + (data[2]<<8)
+                concPM2_5_CF1 = data[5] + (data[4]<<8)
+                concPM10_0_CF1 =data[7] + (data[6]<<8)
+                # Atmospheric particulate values in ug/m3
+                concPM1_0_ATM = data[9] + (data[8]<<8)
+                concPM2_5_ATM = data[11] + (data[10]<<8)
+                concPM10_0_ATM = data[13] + (data[12]<<8)
+                # Raw counts per 0.1l
+                rawGt0_3um = data[15] + (data[14]<<8)
+                rawGt0_5um = data[17] + (data[16]<<8)
+                rawGt1_0um = data[19] + (data[18]<<8)
+                rawGt2_5um = data[21] + (data[20]<<8)
+                rawGt5_0um = data[23] + (data[22]<<8)
+                rawGt10_0um = data[25] + (data[24]<<8)
+                # Misc data
+                version = data[26]
+                errorCode = data[27]
+                payloadChecksum = data[29] + (data[28]<<8)
 
-			# Calculate the payload checksum (not including the payload checksum bytes)
-			inputChecksum = 0x42 + 0x4d
-			for x in range(0,27):
-				inputChecksum = inputChecksum + ord(data[x])
+                # Calculate the payload checksum (not including the payload checksum bytes)
+                inputChecksum = 0x42 + 0x4d
+                for x in range(0,27):
+                        inputChecksum = inputChecksum + data[x]
 
-			os.system('clear') # Set to 'cls' on Windows
-			print("PMS7003 Sensor Data:")
-			print("PM1.0 = " + str(concPM1_0_CF1) + " ug/m3")
-			print("PM2.5 = " + str(concPM2_5_CF1) + " ug/m3")
-			print("PM10 = " + str(concPM10_0_CF1) + " ug/m3")
-			print("PM1 Atmospheric concentration = " + str(concPM1_0_ATM) + " ug/m3")
-			print("PM2.5 Atmospheric concentration = " + str(concPM2_5_ATM) + " ug/m3")
-			print("PM10 Atmospheric concentration = " + str(concPM10_0_ATM) + " ug/m3")
-			print("Count: 0.3um = " + str(rawGt0_3um) + " per 0.1l")
-			print("Count: 0.5um = " + str(rawGt0_5um) + " per 0.1l")
-			print("Count: 1.0um = " + str(rawGt1_0um) + " per 0.1l")
-			print("Count: 2.5um = " + str(rawGt2_5um) + " per 0.1l")
-			print("Count: 5.0um = " + str(rawGt5_0um) + " per 0.1l")
-			print("Count: 10um = " + str(rawGt10_0um) + " per 0.1l")
-			print("Version = " + str(version))
-			print("Error Code = " + str(errorCode))
-			print("Frame length = " + str(frameLength))
-			if inputChecksum != payloadChecksum:
-				print("Warning! Checksums don't match!")
-				print("Calculated Checksum = " + str(inputChecksum))
-				print("Payload checksum = " + str(payloadChecksum))
-	time.sleep(0.7) # Maximum recommended delay (as per data sheet)
+                os.system('clear') # Set to 'cls' on Windows
+                print("PMS7003 Sensor Data:")
+                print("PM1.0 = " + str(concPM1_0_CF1) + " ug/m3")
+                print("PM2.5 = " + str(concPM2_5_CF1) + " ug/m3")
+                print("PM10 = " + str(concPM10_0_CF1) + " ug/m3")
+                print("PM1 Atmospheric concentration = " + str(concPM1_0_ATM) + " ug/m3")
+                print("PM2.5 Atmospheric concentration = " + str(concPM2_5_ATM) + " ug/m3")
+                print("PM10 Atmospheric concentration = " + str(concPM10_0_ATM) + " ug/m3")
+                print("Count: 0.3um = " + str(rawGt0_3um) + " per 0.1l")
+                print("Count: 0.5um = " + str(rawGt0_5um) + " per 0.1l")
+                print("Count: 1.0um = " + str(rawGt1_0um) + " per 0.1l")
+                print("Count: 2.5um = " + str(rawGt2_5um) + " per 0.1l")
+                print("Count: 5.0um = " + str(rawGt5_0um) + " per 0.1l")
+                print("Count: 10um = " + str(rawGt10_0um) + " per 0.1l")
+                print("Version = " + str(version))
+                print("Error Code = " + str(errorCode))
+                print("Frame length = " + str(frameLength))
+                if inputChecksum != payloadChecksum:
+                        print("Warning! Checksums don't match!")
+                        print("Calculated Checksum = " + str(inputChecksum))
+                        print("Payload checksum = " + str(payloadChecksum))
